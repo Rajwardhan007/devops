236a70e575042aec059b840af47db138b6144b37
Rajwardhan007 <rajwardhanmadane002@gmail.com>
2026-02-06 21:31:07 +0530

    Convert project to Maven structure and compile successfully

---
+++ src/main/java/com/example/Addition.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Addition {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 9;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
